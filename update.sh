b20568d2aef85ef950193dc4911548e9d612e8c6
DamithDDS <32153823+DamithDDS@users.noreply.github.com>
2019-11-22 10:39:41 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#Get the updated images
+docker pull bitnami/odoo:12
+
+#Stop your odoo container
+docker-compose stop odoo
+
+#Take a snapshot of the application state
+mkdir -p /opt/backup/odoo-bkp-$(date +%Y%m%d-%H.%M.%S)/
+rsync -a /opt/cybererp/odoo_data/ /opt/backup/odoo-bkp-$(date +%Y%m%d-%H.%M.%S)/
+
+#Get the updated image postgres
+docker pull bitnami/postgresql:11
+
+#Stop your postgres container
+docker-compose stop postgresql
+
+#Take a snapshot of the application state
+mkdir -p /opt/backup/postgres-bkp-$(date +%Y%m%d-%H.%M.%S)/
+rsync -a /opt/cybererp/postgresql_data/ /opt/backup/postgresql-bkp-$(date +%Y%m%d-%H.%M.%S)
+
+#start the containers
+docker-compose start postgres ; docker-compose start odoo
+
+
