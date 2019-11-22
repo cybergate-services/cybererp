f846f8ff5c8b33ebd7acb4ee0c07a284799c9057
DamithDDS <32153823+DamithDDS@users.noreply.github.com>
2019-11-22 11:06:10 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -20,6 +20,6 @@ mkdir -p /opt/backup/postgres-bkp-$(date +%Y%m%d-%H.%M.%S)/
 rsync -a /opt/cybererp/postgresql_data/ /opt/backup/postgresql-bkp-$(date +%Y%m%d-%H.%M.%S)
 
 #start the containers
-docker-compose start postgres ; docker-compose start odoo
+docker-compose start postgresql ; docker-compose start odoo
 
 
